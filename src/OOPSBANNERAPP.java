79de6b279709f720bdaa32448d338d44132998e0
Hemanth <hemanthsalapu08@gmail.com>
2026-02-22 15:40:21 +0530

    Added UC3 changes

--- src/OOPSBANNERAPP.java
+++ src/OOPSBANNERAPP.java
@@ -2,12 +2,19 @@ public class OOPSBANNERAPP {
 
     public static void main(String[] args) {
 
-        System.out.println("   ***   " + "   ***   " + "  ******  " + "   ***** ");
-        System.out.println("  ** **  " + "  ** **  " + "  **   ** " + "  **   **");
-        System.out.println(" **   ** " + " **   ** " + "  **   ** " + "  **     ");
-        System.out.println(" **   ** " + " **   ** " + "  ******  " + "   ***** ");
-        System.out.println(" **   ** " + " **   ** " + "  **      " + "       **");
-        System.out.println("  ** **  " + "  ** **  " + "  **      " + "  **   **");
-        System.out.println("   ***   " + "   ***   " + "  **      " + "   ***** ");
+        System.out.println(String.join("",
+                "   ***   ", "   ***   ", "  ******  ", "   ***** "));
+        System.out.println(String.join("",
+                "  ** **  ", "  ** **  ", "  **   ** ", "  **   **"));
+        System.out.println(String.join("",
+                " **   ** ", " **   ** ", "  **   ** ", "  **     "));
+        System.out.println(String.join("",
+                " **   ** ", " **   ** ", "  ******  ", "   ***** "));
+        System.out.println(String.join("",
+                " **   ** ", " **   ** ", "  **      ", "       **"));
+        System.out.println(String.join("",
+                "  ** **  ", "  ** **  ", "  **      ", "  **   **"));
+        System.out.println(String.join("",
+                "   ***   ", "   ***   ", "  **      ", "   ***** "));
     }
 }
